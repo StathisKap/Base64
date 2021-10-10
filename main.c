49ac2ee02f2d30671e05860ef978ce8b991ae92d
StathisKap <stathiskap75@gmail.com>
2021-10-10 09:18:19 +0100

    Finished code from onlinegdb

--- main.c
+++ main.c
@@ -3,11 +3,10 @@
 #include <string.h>
 #include <math.h>
 
-#include "TestCode.h"
 
 //**************************
-//***BASE64 encoding table**
-//**************************
+////***BASE64 encoding table**
+////**************************
 static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                                 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
                                 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
@@ -17,71 +16,93 @@ static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                                 'w', 'x', 'y', 'z', '0', '1', '2', '3',
                                 '4', '5', '6', '7', '8', '9', '+', '/'};
 
-char * char_to_binary(unsigned char c)
+char *char_to_binary(unsigned char c)
 {
-    char * letter_byte = malloc(sizeof(char)+1);
-    
-    for( int i = 7; i >= 0; i-- ){
-        letter_byte[7-i] = (char)(( c >> i ) & 1 ? 1 : 0) + '0'; 
+    char *letter_byte = malloc(sizeof(char) + 1);
+
+    for (int i = 7; i >= 0; i--)
+    {
+        letter_byte[7 - i] = (char)((c >> i) & 1 ? 1 : 0) + '0';
     }
     letter_byte[8] = '\0';
     return letter_byte;
 }
 
-char *string_to_binary(const char * Sentence)
+char *string_to_binary(char *Sentence)
 {
-    int len = sizeof(char)*strlen(Sentence)+1;
-    char * Sentence_in_binary = malloc(len);
-    
-    for(size_t i = 0, len = strlen(Sentence); i < len; i++){
-        strcat(Sentence_in_binary,char_to_binary(Sentence[i]));
+    int len = strlen(Sentence) * 8;
+    char *Sentence_in_binary = malloc(len + 1);
+
+    for (size_t i = 0; i < len + 1; i++)
+    {
+        strcat(Sentence_in_binary, char_to_binary(Sentence[i]));
     }
     Sentence_in_binary[len] = '\0';
 
-    // printf("%s\n%ld\n\n",Sentence_in_binary,strlen(Sentence_in_binary));
     return Sentence_in_binary;
 }
 
-char * binary_to_decimal(int n)
+int binary_to_decimal(int n)
 {
     int dec = 0, i = 0, rem = 0;
-    char *string_num = malloc(sizeof(char)*7);
 
-    while (n!=0) {
+    while (n != 0)
+    {
         rem = n % 10;
         n /= 10;
         dec += rem * pow(2, i);
         ++i;
     }
-    sprintf(string_num,"%d",dec);
-    return string_num;
+    return dec;
 }
 
-void binary_string_to_decimal_string(char * Sentence_in_binary)
+char *binary_string_to_base64(char *Sentence_in_binary)
 {
-    char * Sentence_in_decimal = malloc(strlen(Sentence_in_binary));
-    char * six_bit_chunks = malloc(6);
-    
-    for(size_t i = 0, len = strlen(Sentence_in_binary); i < len; i = i + 6){
-        
-        for(size_t j = 0; j < 6; j++)
+    size_t len = strlen(Sentence_in_binary);
+    char *Sentence_in_base64 = malloc(len / 4);
+    char *Sentence_in_base64_start = Sentence_in_base64;
+    char *six_bit_chunks = malloc(6);
+
+    for (size_t i = 0; i < len; i += 6)
+    {
+
+        for (size_t j = 0; j < 6; j++)
         {
-            six_bit_chunks[j] = Sentence_in_binary[i+j];
-            strcat(Sentence_in_decimal,binary_to_decimal(atoi(six_bit_chunks)));
-            strcat(Sentence_in_decimal," ");
-            // printf("\ni: %ld, j: %ld\n",i,j);
+            if (i + j < len)
+            {
+                six_bit_chunks[j] = Sentence_in_binary[i + j];
+                six_bit_chunks[6] = '\0';
+            }
+            else
+            {
+                six_bit_chunks[i + j] = '\0';
+                break;
+            }
         }
-        printf("\n%s\n",six_bit_chunks);
+        *Sentence_in_base64 = encoding_table[binary_to_decimal(atoi(six_bit_chunks))];
+        Sentence_in_base64++;
     }
-    // printf("%s\n%ld\n\n",Sentence_in_decimal,strlen(Sentence_in_decimal));
+
+    return Sentence_in_base64_start;
 }
 
-int main()
+char *base64_Encode(char *Sentence)
 {
-   const char Sentence[] = "This a long sentence that I wanna encode into base64";
-   
-    binary_string_to_decimal_string(string_to_binary(Sentence));
-    string_to_binary(Sentence);
-  
-   return 0;
+    int modulo = strlen(Sentence) % 3;
+    char *encoded_string = malloc(strlen(Sentence) + modulo + 1);
+    strcpy(encoded_string, binary_string_to_base64(string_to_binary(Sentence)));
+    if (modulo > 0)
+        while (modulo--)
+            strcat(encoded_string, "=");
+    return encoded_string;
+}
+
+int main(int argc, char *argv[])
+{
+    char Sentence[] = "Hello there mister fantastic, mister boombastik, dara dara dara duuuuum";
+
+    //   printf("\n%s",base64_Encode(argv[argc]));
+    printf("\n%s", base64_Encode(Sentence));
+
+    return 0;
 }
